62d8b7373bdcae988b0c39d3626c084b99c82f24
suger-free <53336466+suger-free@users.noreply.github.com>
2020-09-27 13:41:10 +0800

    Add files via upload
    https://www.cnblogs.com/xwj517537691/p/4658308.html 根据这一个文档来创建
    完成的是第三个任务（木马的基本功能）

---
+++ programhide/programhide/FileOpetation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programhide
+{
+    class FileOpetation
+    {
+        /// <summary>  
+        /// 保存至本地文件  
+        /// </summary>  
+        /// <param name="ETMID"></param>  
+        /// <param name="content"></param>  
+        public static void SaveRecord(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                return;
+            }
+
+
+            FileStream fileStream = null;
+
+
+            StreamWriter streamWriter = null;
+
+
+            try
+            {
+                string path = Path.Combine(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase, string.Format("{0:yyyyMMdd}", DateTime.Now));
+
+
+                using (fileStream = new FileStream(path, FileMode.Append, FileAccess.Write))
+                {
+                    using (streamWriter = new StreamWriter(fileStream))
+                    {
+                        streamWriter.Write(content);
+
+
+                        if (streamWriter != null)
+                        {
+                            streamWriter.Close();
+                        }
+                    }
+
+
+                    if (fileStream != null)
+                    {
+                        fileStream.Close();
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
---
+++ programhide/programhide/ldService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace programhide
+{
+    public partial class ldService : ServiceBase
+    {
+        System.Threading.Timer recordTimer;
+        public ldService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            IntialSaveRecord();
+        }
+
+        protected override void OnStop()
+        {
+            if (recordTimer != null)
+            {
+                recordTimer.Dispose();
+            }
+        }
+        private void IntialSaveRecord()
+        {
+            TimerCallback timerCallback = new TimerCallback(CallbackTask);
+
+
+            AutoResetEvent autoEvent = new AutoResetEvent(false);
+
+
+            recordTimer = new System.Threading.Timer(timerCallback, autoEvent, 10000, 60000 * 10);
+        }
+
+
+        private void CallbackTask(Object stateInfo)
+        {
+            FileOpetation.SaveRecord(string.Format(@"当前记录时间：{0},程序状况：木马运行正常！", DateTime.Now));
+        }
+    }
+}
